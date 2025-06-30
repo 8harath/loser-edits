7cb4dad7f2785ed7902b5c0d3b6d173694a669e6
Bharath K <8harath.k@gmail.com>
2025-06-30 17:50:22 +0530

    Made chnages to the arrow which indicates to scroll and removed it

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -112,22 +112,6 @@ export default function HeroSection() {
           </motion.a>
         </motion.div>
       </div>
-
-      {/* Scroll indicator */}
-      <motion.div
-        className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, delay: 2.5 }}
-      >
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ duration: 2, repeat: Number.POSITIVE_INFINITY }}
-          className="text-dust-grey"
-        >
-          <ChevronDown size={32} />
-        </motion.div>
-      </motion.div>
     </section>
   )
 }
